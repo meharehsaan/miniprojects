9f96dd30dfd9f1c8b3aa1a079ac62c93c0c4f8fb
meharehsaan <ehsmehar@gmail.com>
2023-06-15 21:29:45 +0500

    Added Typescript

---
+++ Typescript-Basic-Ex/Q11.ts
@@ -0,0 +1,5 @@
+let names: string[] = ["Alice", "Bob", "Charlie", "Dave"];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
---
+++ Typescript-Basic-Ex/Q3.ts
@@ -0,0 +1,4 @@
+let myName: string = "John Doe";
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase()); 
+// console.log(name.charAt(0).toUpperCase() + name.substr(1).toLowerCase());
